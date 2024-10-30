1ac1fd26e41b720ecaff97a6c00c5f39e483d51c
Ben Hong <benhong928@gmail.com>
2024-10-30 19:56:16 -0400

    bens changes

--- biasdetector/src/benspage/benswork.jsx
+++ biasdetector/src/benspage/benswork.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './benswork.css';
 
 const Ben = () => {
   return (
-    <div style={{ backgroundColor: 'blue', height: '100vh', color: 'white' }}>
-      <h1>Welcome to My Blue Page!</h1>
-      <p>This is a simple React page with a blue background.</p>
+    <div class = "Main">
+      <h1>Welcome to Bens Page</h1>
+      <p1>This is a simple React page with a blue background.</p1>
+      <p2>lorem</p2>
     </div>
+
   );
 };
 
