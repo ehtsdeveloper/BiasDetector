b85b5df8f84f5ca01296f8129254ad8774216fdd
MusaShoeb <mshoeb@umich.edu>
2024-10-30 19:56:24 -0400

    updated MusaShoeb

--- biasdetector/src/musaspage/musashoeb.jsx
+++ biasdetector/src/musaspage/musashoeb.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './musashoeb.css'
+
 
 const Musa = () => {
   return (
-    <div style={{ backgroundColor: 'blue', height: '100vh', color: 'white' }}>
-      <h1>Welcome to My Blue Page!</h1>
-      <p>This is a simple React page with a blue background.</p>
+    <div class = "Background">
+      <h1>This is Musa's Page (Glorious, Wondrous, Magnificent, Leader is He)!</h1>
+      <div class = "Certify"> Certified doctorate in Rizzology </div>
     </div>
   );
 };
