cb1afbece37ffce906776e7a2a66d6750e6cbe4b
Ben Hong <benhong928@gmail.com>
2024-10-30 20:33:31 -0400

    commit

--- biasdetector/src/main.jsx
+++ biasdetector/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Ben from './benspage/benswork.jsx'
 
 
 createRoot(document.getElementById('root')).render(
