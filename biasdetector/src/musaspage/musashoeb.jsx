c733612e801f4b7bc9cc8b03a2c069bbdd131d47
MusaShoeb <mshoeb@umich.edu>
2024-10-30 20:02:42 -0400

    Created new branch

--- biasdetector/src/musaspage/musashoeb.jsx
+++ biasdetector/src/musaspage/musashoeb.jsx
@@ -5,7 +5,7 @@ import './musashoeb.css'
 const Musa = () => {
   return (
     <div class = "Background">
-      <h1>This is Musa's Page (Glorious, Wondrous, Magnificent, Leader is He)!</h1>
+      <h1>This is Musa's Page 1 (Glorious, Wondrous, Magnificent, Leader is He)!</h1>
       <div class = "Certify"> Certified doctorate in Rizzology </div>
     </div>
   );
