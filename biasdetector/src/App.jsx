906d2742f372ee1de852247b27f9bec4c6ceacfe
MusaShoeb <mshoeb@umich.edu>
2024-11-06 14:28:53 -0500

    Fixed app

--- biasdetector/src/App.jsx
+++ biasdetector/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from '../src/components/Home/Home';
+import Login from '../src/components/Login/Login';
+import Report from '../src/components/Report/Report';
 import Header from './components/Header/Header';
 
 
@@ -11,7 +13,7 @@ const App = () => {
           <main>
               <Routes>
                   <Route path="/" element={<Home/>} />
-                  <Route path="/Login" element={<Login />} />
+                  <Route path="/Login" element={<Login/>} />
                   <Route path="/Report" element={<Report/>} />
               </Routes>
               
