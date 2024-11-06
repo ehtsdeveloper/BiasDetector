9165311c88afc866eb7f52f40d81b12a8b8dc87a
Ben Hong <benhong928@gmail.com>
2024-11-06 14:18:34 -0500

    changes

--- biasdetector/src/App.jsx
+++ biasdetector/src/App.jsx
@@ -1,16 +1,25 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from '/Home/Home'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from '../src/components/Home/Home';
+import Login from '../src/components/Login/Login';
+import Report from '../src/components/Report/Report';
+import Header from './components/Header/Header';
 
-function App() {
+
+const App = () => {
   return (
-    <Router>
-    <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-    </div>
-    </Router>
+      <Router>
+          <Header />
+          <main>
+              <Routes>
+                  <Route path="/" element={<Home/>} />
+                  <Route path="/Login" element={<Login />} />
+                  <Route path="/Report" element={<Report/>} />
+              </Routes>
+              
+          </main>
+      </Router>
   );
-}
+};
 
 export default App;
