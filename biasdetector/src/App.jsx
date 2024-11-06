9115a6e09b3d71f2017b81d9ba6849e207bbcf26
Ben Hong <benhong928@gmail.com>
2024-11-06 14:19:34 -0500

    file creation done

--- biasdetector/src/App.jsx
+++ biasdetector/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from '../src/components/Home/Home';
-import Login from '../src/components/Login/Login';
-import Report from '../src/components/Report/Report';
 import Header from './components/Header/Header';
 
 
