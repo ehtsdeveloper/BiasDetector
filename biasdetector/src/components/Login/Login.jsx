19e357fec0003cc6c7b1ffb3ed9a4a55cc5a11f8
Ben Hong <benhong928@gmail.com>
2024-11-26 17:30:05 -0500

    Login Page Branch

--- biasdetector/src/components/Login/Login.jsx
+++ biasdetector/src/components/Login/Login.jsx
@@ -13,19 +13,16 @@ const Login = () => {
                     <h2>Welcome</h2>
                     <form>
                     {/* EMAIL */}
-                        <div className = "login-credentials">
-                            <label htmlFor = "email">Email Address</label>
-                            <input type="email" id="email" name="email" required />
+                    <label htmlFor = "email">Email Address</label>
+                    <input type="email" id="email" name="email" required />
 
-                            <label htmlFor = "password">Password</label>
-                            <input type="password" id="password" name="password" required />
-                        </div>
+                    <label htmlFor = "password">Password</label>
+                    <input type="password" id="password" name="password" required />
+                     
                     {/* FORGOT PASSWORD & LOGIN BUTTONS */}
-                        <div className = "login-actions">
-                            <button type="button" className="password-button">Forgot Password?</button>
-                            <button type="submit" className="login-button">Login</button>
-                            <button type="button" className="create-button">Create an Account</button>
-                        </div>
+                    <button type="submit" className="login-button">Login</button>
+                    <button type="button" className="password-button">Forgot Password?</button>
+                    <button type="button" className="create-button">Create an Account</button>   
                     </form>
                 </div>
             </div>
